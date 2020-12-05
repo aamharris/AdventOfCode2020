f84bcaee391448dd3af062e806bd7938b9f25530
Aaron Harris <aamharris@gmail.com>
2020-12-05 16:17:34 -0600

    clean up

--- Day1/day_1_solved.rb
+++ Day1/day_1_solved.rb
@@ -1,19 +1,4 @@
 require './lib/account_expense_utility'
-# def get_expense_pair(expenses, total)
-#     expensePair = nil
-#     expenses.each do |expense|
-#         remainder = total - expense
-#         expenses.shift()
-#         has_remainder = expenses.include?(remainder)
-
-#         if has_remainder
-#             expensePair = [expense, remainder]
-#             break
-#         end
-#     end
-
-#     return expensePair
-# end
 
 util = AccountExpenseUtility.new
 expenses = util.get_account_expense_transations('./entries.txt')
